c15e0ed090b8f25fe4a54fe7c4ea42c317daead6
kaaps <joao.guimaraes.ribeiro@gmail.com>
2023-04-19 15:49:14 +0100

    fix: remove dot env

--- src/lib/aws.server.ts
+++ src/lib/aws.server.ts
@@ -1,21 +1,21 @@
+import {
+  AWS_ACCESS_KEY_ID,
+  AWS_REGION,
+  AWS_SECRET_ACCESS_KEY
+} from '$env/static/private';
 import { S3Client } from '@aws-sdk/client-s3';
-import 'dotenv';
 
 export const getS3Client = () => {
-  if (
-    !process.env.AWS_REGION ||
-    !process.env.AWS_ACCESS_KEY_ID ||
-    !process.env.AWS_SECRET_ACCESS_KEY
-  ) {
+  if (!AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
     throw new Error(
       'Missing required env variables: AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY'
     );
   }
   return new S3Client({
-    region: process.env.AWS_REGION,
+    region: AWS_REGION,
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY
     }
   });
 };
