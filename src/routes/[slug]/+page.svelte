5aa357a52d17dd11d78401fa1d631987a49924f2
kaaps <joao.guimaraes.ribeiro@gmail.com>
2023-04-19 14:19:15 +0100

    fix: onmount

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,15 +3,25 @@
   import { goto } from '$app/navigation';
   import type { Team } from '$lib/linear';
   import { linearTeams } from '$lib/stores/linearTeams';
+  import { toast } from '@significa/svelte-ui';
+  import { onMount } from 'svelte';
 
   export let data: Team;
 
-  if (data.key) {
-    linearTeams.updateSore(data);
-    if (browser) {
+  onMount(() => {
+    if (data.key) {
+      linearTeams.updateSore(data);
+      toast.success({
+        message: 'Team successfully migrated.'
+      });
+      goto('/');
+    } else {
+      toast.error({
+        message: 'Server Error',
+        description: 'Could not find the team, please try again.',
+        timeout: 0
+      });
       goto('/');
     }
-  } else if (browser) {
-    goto('/');
-  }
+  });
 </script>
