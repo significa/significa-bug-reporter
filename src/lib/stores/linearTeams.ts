7b2f7d35e2386ec2b478b00fe49877ef8fdc4f4b
kaaps <joao.guimaraes.ribeiro@gmail.com>
2023-04-19 14:05:16 +0100

    feat: allow old links

--- src/lib/stores/linearTeams.ts
+++ src/lib/stores/linearTeams.ts
@@ -60,7 +60,15 @@ const createTheme = () => {
         throw new Error();
       }
     },
-    clearStore: () => update(() => [])
+    clearStore: () => update(() => []),
+    updateSore: (team: Team) =>
+      update((prev) => {
+        const exists = prev.find((val) => val.id === team.id);
+        if (exists) {
+          return [...prev];
+        }
+        return [...prev, team];
+      })
   };
 };
 
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
+  import type { Team } from '$lib/linear';
+  import { linearTeams } from '$lib/stores/linearTeams';
+
+  export let data: Team;
+
+  if (data.key) {
+    linearTeams.updateSore(data);
+    if (browser) {
+      goto('/');
+    }
+  } else if (browser) {
+    goto('/');
+  }
+</script>
